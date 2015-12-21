812f6d6a9ee469728fc10f05cb3a639d5cfd6096
gtopolyai <gerely01@gmail.com>
2015-12-21 22:09:17 +0100

    format

--- app/src/main/java/com/topolyai/vlogger/FormatUtil.java
+++ app/src/main/java/com/topolyai/vlogger/FormatUtil.java
@@ -1,18 +1,17 @@
 package com.topolyai.vlogger;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class FormatUtil {
 
     public static String format(String message, Object... args) {
-
-        if (args == null ||args.length == 0) {
+        if (message == null) {
+            return "null";
+        }
+        if (args == null || args.length == 0) {
             return message;
         }
 
         for (Object arg : args) {
-            message = message.replace("{}", arg.toString());
+            message = message.replaceFirst("\\{\\}", arg.toString());
         }
 
         return message;
